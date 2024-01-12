57b666a45899a7f81d45941ada26cf624b4a9056
kenje4090 <kenje4090@gmail.com>
2024-01-12 18:19:51 +0800

    Add using of socks proxy when sending to telegram

--- cli/Watcher/telegrambot/message.go
+++ cli/Watcher/telegrambot/message.go
@@ -1,7 +1,10 @@
 package telegrambot
 
 import (
+	"context"
 	"fmt"
+	"golang.org/x/net/proxy"
+	"net"
 	"net/http"
 )
 
@@ -10,12 +13,21 @@ const (
 )
 
 func SendMessage(botToken, chatID, msg string) error {
+	// create client
+	proxyUrl := "192.168.3.31:8388"
+	dialer, err := proxy.SOCKS5("tcp", proxyUrl, nil, proxy.Direct)
+	dialContext := func(ctx context.Context, network, address string) (net.Conn, error) {
+		return dialer.Dial(network, address)
+	}
+	transport := &http.Transport{DialContext: dialContext,
+		DisableKeepAlives: true}
+	client := &http.Client{Transport: transport}
+
 	req, err := http.NewRequest("POST", fmt.Sprintf(SendMessageFormat, botToken, chatID, msg), nil)
 	if err != nil {
 		return fmt.Errorf("error in creating http request: %v", err)
 	}
 
-	client := http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("error in executing http request: %v", err)
